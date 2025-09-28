759e25b7e13c55bcd3ac4e227e4f9f7610e20ee6
huukien2002 <kienlh@mumesoft.vn>
2025-09-28 22:05:50 +0700

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Collection",
-  description: "Collection",
+  title: "Blog",
+  description: "Blog",
 };
 
 export default function RootLayout({
