4f3d0eb383de9d332d3d30baa8b64656d7f00738
huukien2002 <kienlh@mumesoft.vn>
2025-10-01 21:58:34 +0700

    Update

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -181,7 +181,7 @@ const Header = () => {
   return (
     <AppBar position="static" color="primary">
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Typography variant="h6" component="div">
+        <Typography onClick={() => router.push("/")} variant="h6" component="div" sx={{ cursor: "pointer" }}>
           Blog
         </Typography>
 
